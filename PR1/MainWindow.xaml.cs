237b186c3c5e3c97b2bb008bd3cb4fa9726d1b40
Gratix0 <oganisyan.sanya1@mail.ru>
2023-03-18 12:35:17 +0300

    1

---
+++ PR1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PR1.PostOfficeDataSetTableAdapters;
+
+namespace PR1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OfficeTableAdapter TableOffice = new OfficeTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            AllDataO.ItemsSource = TableOffice.GetData();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            object id_O = (AllDataO.SelectedItem as DataRowView).Row[0];
+            TableOffice.DeleteQuery(Convert.ToInt32(id_O));
+        }
+        //}
+    }
+}
---
+++ PR1/PostOfficeDataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace PR1
+{
+
+
+    partial class PostOfficeDataSet
+    {
+        partial class OfficeDataTable
+        {
+        }
+    }
+}
+
+namespace PR1.PostOfficeDataSetTableAdapters {
+    
+    
+    public partial class OfficeTableAdapter {
+    }
+}
